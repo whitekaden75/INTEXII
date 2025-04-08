1e9d6bb450350989bfc9c9d92d7d406d9242253a
dalschuetz <131074445+dalschuetz@users.noreply.github.com>
2025-04-08 13:51:26 -0600

    fixed the routing, authorize view for some, need to fix rendering

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df6b6440b4926773164455d891cf207d73688511")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23957163bfc54141d2fd293d6665eea14a62851c")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
