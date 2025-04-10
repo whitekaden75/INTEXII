831cc2f383fb15f0ab210b2eb66713e78b70ae1b
dalschuetz <131074445+dalschuetz@users.noreply.github.com>
2025-04-10 12:41:04 -0600

    make sure everything is deployed

--- frontend/src/pages/MovieDetail.tsx
+++ frontend/src/pages/MovieDetail.tsx
@@ -109,6 +109,7 @@ const MovieDetail = () => {
   }
   const safeTitle = movie.title.replace(/[:'&-()!?.]/g, "");
   const defaultPosterUrl = `https://intex212.blob.core.windows.net/movie-posters/${safeTitle}.jpg`;
+  //this comment is to just make sure everything is working
 
   return (
     <Layout>
