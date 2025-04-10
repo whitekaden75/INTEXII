f1aabaaf73296debf77a8a57bb81ecafa3657e1b
dalschuetz <131074445+dalschuetz@users.noreply.github.com>
2025-04-10 11:55:56 -0600

    fixed thing

--- backend/Controllers/MovieController.cs
+++ backend/Controllers/MovieController.cs
@@ -18,40 +18,31 @@ public class MoviesController : ControllerBase
     }
 
     // GET: api/movies
+    // GET: api/movies
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Movie>>> GetMovies(
-        [FromQuery] int page = 1,
-        [FromQuery] int pageSize = 20,
         [FromQuery] string search = null,
         [FromQuery] string genre = null)
     {
-        if (page <= 0) page = 1;
-        if (pageSize <= 0) pageSize = 20;
-    
         var query = _context.Movies.AsQueryable();
-    
+
         if (!string.IsNullOrEmpty(search))
         {
             query = query.Where(m => m.Title.Contains(search));
         }
-    
+
         if (!string.IsNullOrEmpty(genre))
         {
             query = query.Where(m => m.Genre.Contains(genre));
         }
-    
+
         var totalCount = await query.CountAsync();
-    
-        var movies = await query
-            .Skip((page - 1) * pageSize)
-            .Take(pageSize)
-            .ToListAsync();
-    
         Response.Headers.Add("X-Total-Count", totalCount.ToString());
-    
+
+        var movies = await query.ToListAsync();
         return movies;
     }
-
+    
     // GET: api/movies/s1
     [HttpGet("{id}")]
     public async Task<ActionResult<Movie>> GetMovie(string id)
